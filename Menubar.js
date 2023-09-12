cb31698b51c7f9fa11b1b0066481c88949ffe1cc
Phakkhanan Phukphung <69714175+Nayton2550@users.noreply.github.com>
2023-09-12 21:49:41 +0700

    Update Menubar.js

--- Menubar.js
+++ Menubar.js
@@ -1,8 +1,6 @@
 function onOpen(e) {
   var ui = SpreadsheetApp.getUi();
   ui.createMenu('Set Triggers')
-      .addItem('Every 10 Minute', 'minuteTrigger10')
-      .addSeparator()
       .addItem('Every 1 Hours', 'HoursTrigger1')
       .addSeparator()
       .addItem('delete ALL Trigger', 'deleteTrigger')
